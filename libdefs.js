2f89013340fb25a4bd49a472981f77d1424918c1
Dominic Torruellas <dtorruellas@gmail.com>
2017-06-15 22:28:30 -0700

    Init commit

---
+++ libdefs.js
@@ -0,0 +1,5 @@
+declare module 'express' {  declare var exports: any;  }
+declare module 'compression' {  declare var exports: any;  }
+declare module 'babel-polyfill' {  declare var exports: any;  }
+declare module 'react-hot-loader' {  declare var exports: any;  }
+declare module 'webpack' {  declare var exports: any;  }
---
+++ src/client/app.jsx
@@ -0,0 +1,7 @@
+// @flow
+
+import React from 'react'
+
+const App = () => <h1>Hello World from React with Hot Loading!</h1>
+
+export default App

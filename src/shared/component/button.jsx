a6d8042f8ae8e21f005031fce81a25b6e222f925
Dominic Torruellas <dtorruellas@gmail.com>
2017-06-17 13:22:10 -0700

    Added jss

--- src/shared/component/button.jsx
+++ src/shared/component/button.jsx
@@ -7,12 +7,13 @@ type Props = {
   handleClick: Function,
 }
 
-const Button = ({ label, handleClick }: Props) =>
+const Button = ({ label, handleClick }: Props) => (
   <button
   onClick={handleClick}
   className="btn btn-primary"
   type="button"
   role="button"
   >{label}</button>
+)
 
 export default Button
